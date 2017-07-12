6f4d3f4f0ca9c2ff9da92089c6f5562cc6727913
tripott <tripott@gmail.com>
2017-07-12 16:47:08 -0400

    dal refactor

--- dal.js
+++ dal.js
@@ -13,39 +13,19 @@ const { append, find, reject, compose, trim } = require('ramda')
 function addCat(cat, callback) {
   // example _id -- "cat_big_time_owner_333"
   cat._id = pkGenerator('cat_', trim(cat.name) + ' ' + trim(cat.ownerId))
-
-  db.put(cat, function(err, doc) {
-    if (err) callback(err)
-    callback(null, doc)
-  })
+  add(cat, callback)
 }
 
 function getCat(catId, callback) {
-  db.get(catId, function(err, doc) {
-    if (err) callback(err)
-    callback(null, doc)
-  })
+  get(catId, callback)
 }
 
 function updateCat(updatedCat, callback) {
-  db.put(updatedCat, function(err, doc) {
-    if (err) callback(err)
-    callback(null, doc)
-  })
+  update(updatedCat, callback)
 }
 
 function deleteCat(catId, callback) {
-  db
-    .get(catId)
-    .then(function(doc) {
-      return db.remove(doc)
-    })
-    .then(function(result) {
-      callback(null, result)
-    })
-    .catch(function(err) {
-      callback(err)
-    })
+  deleteDoc(catId, callback)
 }
 
 function listCats(limit, callback) {
@@ -71,39 +51,19 @@ function listCats(limit, callback) {
 function addBreed(breed, callback) {
   // example _id -- "breed_pixie-bob"
   breed._id = pkGenerator('breed_', trim(breed.breed))
-
-  db.put(breed, function(err, doc) {
-    if (err) callback(err)
-    callback(null, doc)
-  })
+  add(breed, callback)
 }
 
 function getBreed(breedId, callback) {
-  db.get(breedId, function(err, doc) {
-    if (err) callback(err)
-    callback(null, doc)
-  })
+  get(breedId, callback)
 }
 
 function updateBreed(updatedBreed, callback) {
-  db.put(updatedBreed, function(err, doc) {
-    if (err) callback(err)
-    callback(null, doc)
-  })
+  update(updatedBreed, callback)
 }
 
 function deleteBreed(breedId, callback) {
-  db
-    .get(breedId)
-    .then(function(doc) {
-      return db.remove(doc)
-    })
-    .then(function(result) {
-      callback(null, result)
-    })
-    .catch(function(err) {
-      callback(err)
-    })
+  deleteDoc(breedId, callback)
 }
 
 function listBreeds(limit, callback) {
@@ -131,6 +91,41 @@ function list(options, callback) {
     if (err) callback(err)
     callback(null, map(row => row.doc, data.rows))
   })
+}
+
+function add(doc, callback) {
+  db.put(doc, function(err, doc) {
+    if (err) callback(err)
+    callback(null, doc)
+  })
+}
+
+function get(id, callback) {
+  db.get(id, function(err, doc) {
+    if (err) callback(err)
+    callback(null, doc)
+  })
+}
+
+function update(doc, callback) {
+  db.put(doc, function(err, doc) {
+    if (err) callback(err)
+    callback(null, doc)
+  })
+}
+
+function deleteDoc(id, callback) {
+  db
+    .get(id)
+    .then(function(doc) {
+      return db.remove(doc)
+    })
+    .then(function(result) {
+      callback(null, result)
+    })
+    .catch(function(err) {
+      callback(err)
+    })
 }
 
 const dal = {
